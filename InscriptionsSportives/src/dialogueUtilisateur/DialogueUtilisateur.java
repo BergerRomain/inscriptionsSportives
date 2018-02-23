6fe77d2977e8f96205daf8d75533e12681606f3c
Romain <Romain@Romain>
2018-02-23 15:12:04 +0100

    Finilisation des parties equipe et personne

--- InscriptionsSportives/src/dialogueUtilisateur/DialogueUtilisateur.java
+++ InscriptionsSportives/src/dialogueUtilisateur/DialogueUtilisateur.java
@@ -1,17 +1,19 @@
 package dialogueUtilisateur;
 
-import static commandLineMenus.rendering.examples.util.InOut.getString;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import commandLineMenus.*;
-import commandLineMenus.examples.employees.core.*;
+import inscriptions.*;
+import static commandLineMenus.rendering.examples.util.InOut.*;
 
 public class DialogueUtilisateur 
 {
-	private ManageEmployees inscriptionSportive;
+	private Inscriptions inscriptions;
 	
-	public DialogueUtilisateur(ManageEmployees inscriptionSportive)
+	public DialogueUtilisateur(Inscriptions inscriptions)
 	{
-		this.inscriptionSportive = inscriptionSportive;
+		this.inscriptions = inscriptions;
 	}
 	
 	public void start()
@@ -23,31 +25,13 @@ public class DialogueUtilisateur
 	{
 		Menu inscriptionMenu = new Menu("Inscription sportive");
 		inscriptionMenu.add(Inscription());
-		inscriptionMenu.add(superUser(inscriptionSportive.getRoot()));
 		inscriptionMenu.add(quitter());
 		return inscriptionMenu;
 	}
 	
-	private Menu superUser(Employee root)
-	{
-		Menu superUser = new Menu("Gerer le super utilisateur", "2");
-		//superUser.add();
-		//superUser.add();
-		//superUser.add();
-		//superUser.add();
-		superUser.add(modifierMDP(root));
-		superUser.addBack("6");
-		return superUser;
-	}
-	
-	private Option modifierMDP(final Employee root)
-	{
-		return new Option("Changer le mot de pasee", "5", () -> {root.setPassword(getString("Nouveau mot de passe : "));});
-	}
-	
 	private Menu quitter()
 	{
-		Menu quitter = new Menu("Quitter", "3");
+		Menu quitter = new Menu("Quitter", "2");
 		quitter.add(quitterEtEnregistrer());
 		quitter.add(quitterSansEnregistrer());
 		quitter.addBack("3");
@@ -61,11 +45,10 @@ public class DialogueUtilisateur
 				{
 					try
 					{
-						inscriptionSportive.sauvegarder();
+						inscriptions.sauvegarder();
 						Action.QUIT.optionSelected();
 					} 
-					catch (ImpossibleToSaveException e)
-					{
+					catch (IOException e) {
 						System.out.println("Impossible d'effectuer la sauvegarde");
 					}
 				}
@@ -89,11 +72,16 @@ public class DialogueUtilisateur
 	private Menu menuCompetition()
 	{
 		Menu menuCompetition = new Menu("Gerer les competitions", "1");
-		//menuCompetition.add(afficherCompetition());
+		menuCompetition.add(afficherCompetition());
 		//menuCompetition.add(ajouterCompetition());
-		//menuCompetition.add(editerCompetition());
+		//menuCompetition.add(selectionnerCompetition());
 		menuCompetition.addBack("4");
 		return menuCompetition;
+	}
+	
+	private Option afficherCompetition()
+	{
+		return new Option("Afficher les competitions", "1", () -> {System.out.println(inscriptions.getCompetitions());});
 	}
 	
 	private Menu candidat()
@@ -108,35 +96,170 @@ public class DialogueUtilisateur
 	private Menu menuEquipe()
 	{
 		Menu menuEquipe = new Menu("Gerer les equipes", "1");
-		//menuEquipe.add(afficherEquipe());
-		//menuEquipe.add(ajouterEquipe());
-		//menuEquipe.add(editerEquipe());
+		menuEquipe.add(afficherEquipe());
+		menuEquipe.add(ajouterEquipe());
+		menuEquipe.add(selectionnerEquipe());
 		menuEquipe.addBack("4");
 		return menuEquipe;
+	}
+	
+	private Option afficherEquipe()
+	{
+		return new Option("Afficher les equipes", "1", () -> {System.out.println(inscriptions.getEquipes());});
+	}
+	
+	private Option ajouterEquipe()
+	{
+		return new Option("Ajouter une equipe", "2", () -> inscriptions.createEquipe(getString("Nom : ")));
+	}
+	
+	private List<Equipe> selectionnerEquipe()
+	{
+		return new List<Equipe>("Selectionner une equipe", "3", 
+				() -> new ArrayList<>(inscriptions.getEquipes()),
+				(element) -> editerEquipe(element)
+				);
+	}
+	
+	private Menu editerEquipe(Equipe equipe)
+	{
+		Menu menu = new Menu("Editer " + equipe.getNom());
+		menu.add(afficher(equipe));
+		menu.add(modifierNom(equipe));
+		menu.add(supprimer(equipe));
+		menu.add(afficherMembre(equipe));
+		menu.add(ajouterMembre(equipe));
+		menu.add(supprimerMembre(equipe));
+		menu.addBack("7");
+		return menu;
+	}
+	
+	private Option afficher(Equipe equipe)
+	{
+		return new Option("Afficher l'equipe", "1", 
+				() -> 
+				{
+					System.out.println("Nom : " + equipe.getNom());
+				}
+		);
+	}
+	
+	private Option modifierNom(Equipe equipe)
+	{
+		return new Option("Modifier le nom", "2", 
+				() -> {equipe.setNom(getString("Nouveau nom : "));});
+	}
+	
+	private Option supprimer(Equipe equipe)
+	{
+		return new Option("Supprimer", "3", () -> {equipe.delete();});
+	}
+	
+	private Option afficherMembre(Equipe equipe)
+	{
+		return new Option("Afficher les membres", "4", () -> {System.out.println(equipe.getMembres());});
+	}
+	
+	private List<Personne> ajouterMembre(final Equipe equipe)
+	{
+		return new List<>("Ajouter un membre", "5", 
+				() -> new ArrayList<>(inscriptions.getPersonnes()),
+				(index, element) -> {equipe.add(element);}
+				);
+	}
+	
+	private List<Personne> supprimerMembre(final Equipe equipe)
+	{
+		return new List<>("Supprimer un membre", "6", 
+				() -> new ArrayList<>(inscriptions.getPersonnes()),
+				(index, element) -> {equipe.remove(element);}
+				);
 	}
 	
 	private Menu menuPersonne()
 	{
 		Menu menuPersonne = new Menu("Gerer les personnes", "2");
-		//menuPersonne.add(afficherPersonne());
-		//menuPersonne.add(ajouterPersonne());
-		//menuPersonne.add(editerPersonne());
-		menuPersonne.addBack("4");
+		menuPersonne.add(afficherPersonne());
+		menuPersonne.add(ajouterPersonne());
+		menuPersonne.add(selectionnerPersonne());
+		menuPersonne.addBack("4");	
 		return menuPersonne;
 	}
 	
-	private boolean verifiePassword()
+	private Option afficherPersonne()
 	{
-		boolean ok = inscriptionSportive.getRoot().checkPassword(getString("Mot de passe : "));
-		if (!ok)
-			System.out.println("Mot de passe incorrect");
-		return ok;
+		return new Option("Afficher les personnes", "1", () -> {System.out.println(inscriptions.getPersonnes());});
+	}
+	
+	private Option ajouterPersonne()
+	{
+		return new Option("Ajouter une personne", "2", () -> inscriptions.createPersonne(getString("Nom : "), getString("Prenom : "), getString("Mail : ")));
+	}
+	
+	private List<Personne> selectionnerPersonne()
+	{
+		return new List<Personne>("Selectionner une personne", "3", 
+				() -> new ArrayList<>(inscriptions.getPersonnes()),
+				(element) -> editerPersonne(element)
+				);
+	}
+	
+	private Menu editerPersonne(Personne personne)
+	{
+		Menu menu = new Menu("Editer " + personne.getNom());
+		menu.add(afficher(personne));
+		menu.add(modifierNom(personne));
+		menu.add(modifierPrenom(personne));
+		menu.add(modifierMail(personne));
+		menu.add(supprimer(personne));
+		menu.add(afficherEquipe(personne));
+		menu.addBack("7");
+		return menu;
+	}
+	
+	private Option afficher(Personne personne)
+	{
+		return new Option("Afficher la personne", "1", 
+				() -> 
+				{
+					System.out.println("Nom : " + personne.getNom());
+					System.out.println("Prenom : " + personne.getPrenom());
+					System.out.println("Mail : " + personne.getMail());
+				}
+		);
+	}
+	
+	private Option modifierNom(Personne personne)
+	{
+		return new Option("Modifier le nom", "2", 
+				() -> {personne.setNom(getString("Nouveau nom : "));});
+	}
+	
+	private Option modifierPrenom(Personne personne)
+	{
+		return new Option("Modifier le prenom", "3", 
+				() -> {personne.setPrenom(getString("Nouveau prenom : "));});
+	}
+	
+	private Option modifierMail (Personne personne)
+	{
+		return new Option("Modifier le mail", "4", 
+				() -> {personne.setMail(getString("Nouveau mail : "));});
+	}
+	
+	private Option supprimer(Personne personne)
+	{
+		return new Option("Supprimer", "5", () -> {personne.delete();});
+	}
+	
+	private Option afficherEquipe(Personne personne)
+	{
+		return new Option("Afficher les equipe", "6", () -> {System.out.println(personne.getEquipes());});
 	}
 	
 	public static void main(String[] args)
 	{
-		DialogueUtilisateur console = new DialogueUtilisateur(ManageEmployees.getManageEmployees());
-		if (console.verifiePassword())
+		DialogueUtilisateur console = new DialogueUtilisateur(Inscriptions.getInscriptions());
 		console.start();
 	}
 }
